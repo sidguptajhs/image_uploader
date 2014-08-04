e4f59498412917037335d19e759dbd67e75765ab
Siddharth Gupta <sgupta@thoughtworks.com>
2014-08-04 22:36:22 +0530

    Testing 2

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,6 @@ module ImageUploader
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.gem "rmagick", :lib => "RMagick"
   end
 end
