ac63c47e19ce6b7ad7380e0fc4dfe289ead01e95
Siddharth Gupta <sgupta@thoughtworks.com>
2014-08-04 23:55:29 +0530

    Another try

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,6 @@
 require 'base64'
-require 'rmagick'
+require 'Rmagick'
+include Magick
 class Image < ActiveRecord::Base
   attr_accessible :source, :username, :data, :thumb_data
   before_save :save_image
